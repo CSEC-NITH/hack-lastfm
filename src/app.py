569081b68cd8e3f66f8616ec9f8fe1982ab3956f
Prakriti Gupta <prakriti.gupta10@gmail.com>
2017-02-05 04:55:57 -0500

    stats added!

--- src/app.py
+++ src/app.py
@@ -2,8 +2,10 @@ from __future__ import print_function
 from flask import Flask
 from flask import request, render_template, send_file
 import config
-from utils import get_token,get_album_art, generate_collage
+from utils import get_token,get_album_art, generate_collage, gen_graph, get_album_data, get_artists_data, get_tracks_data
 import requests
+import json
+
 
 app = Flask(__name__)
 
@@ -13,11 +15,9 @@ def index():
 
 @app.route('/collage', methods= ['GET','POST'])
 def collage():
-    print("HI")
     if request.method=='GET':
         return render_template('collage.html',input=True)
     elif request.method == 'POST':
-        print("HELLO")
         username = request.form['lastfm_username']
         duration = request.form['duration']
         print(username)
@@ -34,6 +34,36 @@ def collage():
         generate_collage(filenames,username)
         return send_file('static/' + username+'.jpg', mimetype='image/jpg')
 
+@app.route('/stats', methods=['GET','POST'])
+def stats():
+    if request.method=='GET':
+        return render_template('stats.html',input=True)
+    elif request.method == 'POST':
+        username = request.form['lastfm_username']
+        duration = request.form['duration']
+        print(username)
+        payload = {
+            'user': username,
+            'api_key': config.LASTFM_API_KEY,
+            'method': 'user.gettopalbums',
+            'period':duration,
+            'limit':10,
+            'format':'json'
+        }
+        r = requests.get(config.url, params = payload)
+        artists_names,album_names,playcounts=get_album_data(r.text)
+        gen_graph("Top Albums",album_names,playcounts,username + "_albums.png")
+        payload['method']='user.gettopartists'
+        r = requests.get(config.url, params = payload)
+        artists_names,playcounts=get_artists_data(r.text)
+        gen_graph("Top Artists",artists_names,playcounts,username + "_artists.png")
+        payload['method']='user.gettoptracks'
+        r = requests.get(config.url, params = payload)
+        track_names,playcounts=get_tracks_data(r.text)
+        gen_graph("Top Tracks",track_names,playcounts,username + "_tracks.png")
+
+        return render_template('stats.html',input=False,username=username)
+        
 if __name__ == "__main__":
     app.run()
 
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,9 @@ import json
 import hashlib
 from PIL import Image
 import shutil
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
 
 def md5sum(string):
     m = hashlib.md5()
@@ -58,8 +61,55 @@ def download_images(links, username):
         cnt += 1
     return filenames  
 
+def get_album_data(response):
+    artist_names=[]
+    album_names=[]
+    playcounts=[]
+    data=json.loads(response)
+    print(json.dumps(data,indent=4))
+    albums=data['topalbums']['album']
+    for album in albums:
+        artist_names.append(album['artist']['name'])
+        album_names.append(album['name'])
+        playcounts.append(int(album["playcount"]))
+    artist_names.reverse()
+    album_names.reverse()
+    playcounts.reverse()
+    return artist_names,album_names,playcounts    
+
+def gen_graph(title,labels,frequency,filename):
+    x_pos=np.arange(len(labels))
+    plt.figure(figsize=(25,10))
+    plt.barh(x_pos, frequency, align='center', alpha=0.5, height=0.5)
+    plt.yticks(x_pos, labels)
+    plt.xlabel('Playcounts')
+    plt.title(title)
+    plt.savefig('static/' + filename)
+
+def get_artists_data(response): 
+    artist_names=[]
+    playcounts=[]
+    data=json.loads(response)
+    print(json.dumps(data,indent=4))
+    artists=data['topartists']['artist']
+    for artist in artists:
+        artist_names.append(artist['name'])
+        playcounts.append(int(artist["playcount"]))
+    artist_names.reverse()
+    playcounts.reverse()
+    return artist_names,playcounts      
 
 
-
-
+def get_tracks_data(response): 
+    track_names=[]
+    playcounts=[]
+    data=json.loads(response)
+    print(json.dumps(data,indent=4))
+    tracks=data['toptracks']['track']
+    for track in tracks:
+        track_names.append(track['name'])
+        playcounts.append(int(track["playcount"]))
+    track_names.reverse()
+    playcounts.reverse()
+    return track_names,playcounts      
 
