043515c8652e3da68eea4ebcad9eeda69dba89f8
Prakriti Gupta <prakriti.gupta10@gmail.com>
2017-02-05 07:25:44 -0500

    Heroku specific stuff

--- app.py
+++ app.py
@@ -137,4 +137,4 @@ def comparison():
             return render_template('compar.html', input = False, graph = user1+user2+'.jpg')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host = '0.0.0.0')
